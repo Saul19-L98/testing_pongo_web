2e57ea56c410f93b5cc4b62f7c1d1641f47ce420
Sallanez <saul.alejandro19@gmail.com>
2024-01-05 17:22:49 -0600

    middle config updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { NextResponse, NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("Cookie");
 
+  //const publicRoutes = ['/','login'];
+
   const url = request.nextUrl.clone();
 
   if (url.pathname.endsWith("/dashboard") && !token) {
@@ -10,3 +12,22 @@ export async function middleware(request: NextRequest) {
   }
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
